65a7dabe5b58b5d3f487cc4bed25eeea24943531
Leo Desktop <leonardozsola1@gmail.com>
2023-04-07 12:02:25 -0400

    installed sidebar and font packages

--- ChatGPTInsta/src/pages/Home.page.js
+++ ChatGPTInsta/src/pages/Home.page.js
@@ -1,7 +1,59 @@
-import { Button } from '@mui/material'
-import { useContext } from 'react';
-import { UserContext } from '../contexts/user.context';
- 
-export default function Home() {
+import './Home.page.css';
+import React from 'react';
+import UserInput from './components/UserInput';
+import InstagramCard from './components/InstagramCard';
 
+import { ProSidebarProvider } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem, sidebarClasses} from 'react-pro-sidebar';
+
+class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          generatedCaption : null,
+          generatedTags : null
+        }
+      }
+
+    render(){
+    return (
+        <div className='GenerationPage'>
+        
+            <div className="SideWrap">
+            <ProSidebarProvider>
+            
+            <Sidebar
+            backgroundColor = {`rgba(33, 33, 33, 1)`} 
+            rootStyles={{
+            [`.${sidebarClasses.container}`]: {
+                color: 'white',
+            },
+            }}
+            >
+            <Menu menuItemStyles={{
+            button: {
+            '&:hover': {
+                backgroundColor: 'rgba(50, 50, 50, 1)',
+            },},
+            }}>
+            <MenuItem> Profile </MenuItem>
+            <MenuItem> Generation </MenuItem>
+            </Menu>
+        </Sidebar>
+        </ProSidebarProvider>
+        </div>
+    
+        <InstagramCard 
+        caption={this.state.generatedCaption}
+        tags={this.state.generatedTags}/>
+    
+        <UserInput 
+        setCaption={(inputCaption)=>{this.setState({generatedCaption : inputCaption})}}
+        setTags={(inputTags)=>{this.setState({generatedTags : inputTags})}}/>
+    
+        </div>
+        );
+    }
 }
+
+export default Home;
---
+++ ChatGPTInsta/src/pages/components/InstagramCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './instagramCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faEllipsisH } from '@fortawesome/free-solid-svg-icons';
+
+import profilePic from '../stockImages/Instagram.png';
+import postPic from '../stockImages/Sunset Boat Florida Stock.png';
+
+class InstagramCard extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        }
+    }
+
+    render(){
+        return (
+            <div className="instagram-card">
+                <div className="instagram-card-header">
+                    <img src={profilePic} className="instagram-card-user-image" alt="User Profile"/>
+                    <a className="instagram-card-user-name">theRealJohnSmith</a>
+                </div>
+
+                <div className="instagram-card-image">
+                    <img src={postPic} alt="Post Image" class="instagram-card-image"/>
+                </div>
+
+                <div className="instagram-card-content">
+                    <p className="likes">Likes: ?</p>
+                    <p><a className="instagram-card-content-user" >theRealJohnSmith</a> 
+                    <a className="instagram-card-content-caption" > {this.props.caption} </a>
+                    <a className="hashtag">{this.props.tags}</a></p>
+                    
+                </div>
+
+            </div>
+        )
+    }
+}
+
+
+export default InstagramCard
