f9260a1d1bc5edea3f438eb68af64e159c612820
Seth <odezro21@gmail.com>
2023-05-04 16:02:14 -0400

    Added guaranteed hashtag function fixed openai bug

--- ChatGPTInsta/app.py
+++ ChatGPTInsta/app.py
@@ -1,6 +1,7 @@
 import os
+import re
 import openai
-from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 
@@ -9,7 +10,7 @@ CORS(app, origins=["http://localhost:3000"])
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
+#OpenAPI requuired function 
 @app.route("/generate", methods=["POST"])
 def generate():
     topic = request.json['topic']
@@ -17,58 +18,27 @@ def generate():
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are an AI that generates Instagram post titles and popular hashtags based on a given topic."},
-            {"role": "user", "content": f"Generate a title and hashtags for a post about {topic}."}
+            {"role": "user", "content": f"Generate a title and 5 hashtags for a post about {topic}."}
         ],
         temperature=0.8,
-        max_tokens=100,
+        max_tokens=150,
     )
     title, hashtags = parse_response(response['choices'][0]['message']['content'])
     return jsonify({'title': title, 'hashtags': hashtags})
 
-
-# def index():
-#     if request.method == "POST":
-#         topic = request.form["topic"]
-#         response = openai.ChatCompletion.create(
-#             model="gpt-3.5-turbo",
-#             messages=[
-#                 {"role": "system", "content": "You are an AI that generates Instagram post titles and popular hashtags based on a given topic."},
-#                 {"role": "user", "content": f"Generate a title and hashtags for a post about {topic}."}
-#             ],
-#             temperature=0.8,
-#             max_tokens=100,
-#         )
-        
-#         print("API RESP?:", response['choices'][0]['message']['content'])
-        
-#         print("parsed title:", title)
-#         print("parsed hashtags:", hashtags)
-#         return render_template("index.html", title=title, hashtags=hashtags)
-#     return render_template("index.html")
-
-
-
-
+#OpenAI API call to generate the title and hashtags
 def generate_prompt(topic):
-    return f"""Generate an engaging title for your Instagram post related to '{topic}' and provide 3 popular hashtags related to '{topic}'.
+    return f"""Generate an engaging title for your Instagram post related to '{topic}' and provide 5 popular hashtags related to '{topic}'.
 
 Title:
 Hashtags:"""
 
+#function that parses the response from the API and makes it readable
 def parse_response(response_text):
-    # print("RESPONSE TEXT:", response_text)
-    lines = response_text.strip().split('Hashtags:')
-    # print("LINES 0:", lines[0])
-    # print("LINES 1:", lines[1])
-    # print(len(lines))
-    # print("LINES:", lines)
-    if(len(lines) < 2):
-        return "Title not generated", "Hashtags not generated"
-    
+    lines = response_text.strip().split('Hashtags:') # split the response into title and hashtags
     title = lines[0].strip("Title:").strip()
-    # print("THIS IS THE TITLE:", title)
     hashtags = lines[1].strip("Hashtags:").strip()
-    # print("THIS IS THE HASHTAGS:", hashtags)
+    hashtags = re.sub(r'\d+\.\s+', '', hashtags) # remove the numbering
     return title, hashtags
 
 if __name__ == "__main__":
